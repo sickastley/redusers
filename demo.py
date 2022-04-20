6ff611f2a370fdd94c75667cc80dba5fffe2dfdb
Aman Pathak <82083977+sickastley@users.noreply.github.com>
2022-04-21 01:13:29 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,18 +3,18 @@ from redusers import Redusers
 client = Redusers()
 
 # to get userlist from existing subs
-# client.bulk('indiaspeaks', 'demo.csv', 1000)
+# client.bulk('indiaspeaks', 'data.csv', 1000)
 
 # to invite  from the given file
-# client.invite('demo.csv')
+# client.invite('data.csv')
 
 # to merge old csv to new type.
 # last value tells whether the file has userbase which has been invited or not.
 # True for invited and False for not invited.
 
-client.oldToNew('pholder_indiaspeaks.csv', 'demo.csv', 'False')
-client.oldToNew('test.csv', 'demo.csv', 'False')
-client.oldToNew('chodi.csv', 'demo.csv', 'False')
-client.oldToNew('users.csv', 'demo.csv', 'False')
+client.oldToNew('pholder_indiaspeaks.csv', 'data.csv', 'False')
+client.oldToNew('test.csv', 'data.csv', 'False')
+client.oldToNew('chodi.csv', 'data.csv', 'False')
+client.oldToNew('users.csv', 'data.csv', 'False')
 
-client.oldToNew('sent.csv', 'demo.csv', 'True')
+client.oldToNew('sent.csv', 'data.csv', 'True')
